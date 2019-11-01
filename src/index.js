96a8481be3d66c0e078503e4df6bb8d8c985a3bc
Emi <emi.bungo@gmail.com>
2019-11-01 13:56:02 -0400

    Finished feature

--- src/index.js
+++ src/index.js
@@ -1,36 +1,9 @@
-const entry = document.querySelector(".app");
+const Deact = require("./libs/deact")
 
-// entry.innerHTML = "<h1>~This is Halloween~</h1>";
-
-const nav =
-  // render(nav, header);
-
-  render(
+render(
     create("header", [
-      create("h1", "This is Halloween ~~"),
-      create("nav", "nav nav nav")
+        create("h1", "This is Halloween ~~"),
+        create("nav", "nav nav nav")
     ]),
-    entry
+    document.querySelector(".app")
   );
-
-function render(child, parent) {
-  parent.append(child);
-}
-
-function create(element, content) {
-  const createdElement = document.createElement(element);
-
-  if (typeof content === "string") {
-    createdElement.innerHTML = content;
-  } else if (content instanceof HTMLElement) {
-    createdElement.append(content);
-  } else if (content instanceof Array)
-    content.forEach(function(htmlElement) {
-    createdElement.append(htmlElement);
-    })
- else {
-    throw new Error("Not a valid content type for element");
-  }
-
-  return createdElement;
-}
