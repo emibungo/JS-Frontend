e4fcd6b395535bcb0b0ff48ba287ab915e7e8d75
Emi <emi.bungo@gmail.com>
2019-11-01 14:25:04 -0400

    Finish nav button

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const Deact = require("./libs/deact")
 
-render(
-    create("header", [
-        create("h1", "This is Halloween ~~"),
-        create("nav", "nav nav nav")
+Deact.render(
+    Deact.create("header", {}, [
+        Deact.create("h1", {}, "This is Halloween ~~"),
+        Deact.create(
+            "nav",
+            {},
+            Deact.create(
+                "button", 
+                {
+                    onclick: function clickButton() {
+                    console.log("Button clicked")
+                    }
+                }, 
+                "Hallow-Hallow-Halloween")
+        )
     ]),
     document.querySelector(".app")
   );
--- src/libs/deact.js
+++ src/libs/deact.js
@@ -1,24 +1,30 @@
 module.exports = {
-    
-render(child, parent) {
+  render(child, parent) {
     parent.append(child);
   },
-  
-create(element, content) {
+
+  create(element, attributes = {}, content) {
     const createdElement = document.createElement(element);
-  
+
+    for (key in attributes) {
+      // createdElement.setAttribute(key, attributes[key]);
+      // console.log(key);
+      // console.log(attributes[key]);
+      createdElement[key] = attributes[key];
+    }
+
     if (typeof content === "string") {
       createdElement.innerHTML = content;
     } else if (content instanceof HTMLElement) {
       createdElement.append(content);
-    } else if (content instanceof Array)
+    } else if (content instanceof Array) {
       content.forEach(function(htmlElement) {
-      createdElement.append(htmlElement);
-      })
-   else {
+        createdElement.append(htmlElement);
+      });
+    } else {
       throw new Error("Not a valid content type for element");
     }
-  
+
     return createdElement;
-  },
-}
+  }
+};
